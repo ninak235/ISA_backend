ab0df15a7424edad7b3b5507fa0e45072f0d546e
aleksandarssrajer <aleksandarssrajer@gmail.com>
2023-11-03 16:18:59 +0100

    Initial Project

---
+++ ISA-Project/src/main/java/com/ISA/ISAProject/IsaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ISA.ISAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaProjectApplication.class, args);
+	}
+
+}
---
+++ ISA-Project/src/test/java/com/ISA/ISAProject/IsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISA.ISAProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
