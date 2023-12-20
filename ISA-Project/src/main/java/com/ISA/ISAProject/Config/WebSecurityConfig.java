126eabd06227afca167c23402909f13f4f391d4c
Nina <zarkokn@gmail.com>
2023-12-20 01:03:53 +0100

    development done

--- ISA-Project/src/main/java/com/ISA/ISAProject/Config/WebSecurityConfig.java
+++ ISA-Project/src/main/java/com/ISA/ISAProject/Config/WebSecurityConfig.java
@@ -81,6 +81,7 @@ public class WebSecurityConfig {
                 .antMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()//SWAGGER
                 .antMatchers("/api/equipment/**").permitAll()
                 .antMatchers(("/api/availableDate/**")).permitAll()
+                .antMatchers(("/api/user/**")).permitAll()
                 //.antMatchers("/api/company/registerCompany").permitAll()
                 // ukoliko ne zelimo da koristimo @PreAuthorize anotacije nad metodama kontrolera, moze se iskoristiti hasRole() metoda da se ogranici
                 // koji tip korisnika moze da pristupi odgovarajucoj ruti. Npr. ukoliko zelimo da definisemo da ruti 'admin' moze da pristupi
