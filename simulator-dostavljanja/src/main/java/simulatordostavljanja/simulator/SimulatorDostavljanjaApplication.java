9ae56f207ed61d5dde07cad664125125b4381f70
Nina <zarkokn@gmail.com>
2024-01-27 22:32:11 +0100

    simulator dostavljanja added

---
+++ simulator-dostavljanja/src/main/java/simulatordostavljanja/simulator/SimulatorDostavljanjaApplication.java
@@ -0,0 +1,13 @@
+package simulatordostavljanja.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorDostavljanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorDostavljanjaApplication.class, args);
+	}
+
+}
